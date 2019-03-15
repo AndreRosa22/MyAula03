5bc1a9600d4c75d6e1d0d1cc74059a11615958a2
AndreRosa22 <andrefrancorosa@gmail.com>
2019-03-15 13:02:53 +0000

    Criei ficheiro exercicio 5

---
+++ ArrayBiDim/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ArrayBiDim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira o número de linhas: ");
+            string r = Console.ReadLine();
+            int row = Convert.ToInt32(r);
+            Console.WriteLine("Insira o número de colunas: ");
+            string c = Console.ReadLine();
+            int columns = Convert.ToInt32(c);
+
+            int[,] matrix = new int[row, columns];
+
+            Console.WriteLine("Insere valor a guardar");
+            string s1 = Console.ReadLine();
+        }
+    }
+}
