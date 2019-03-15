b5d8ce050e8a045ff4d428af1ffb90ece3c28227
AndreRosa22 <andrefrancorosa@gmail.com>
2019-03-15 12:37:22 +0000

    Adicionei solução exercicio 4

---
+++ PrecorreString/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PrecorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char c;
+            //Pede uma string
+            Console.WriteLine("Insere uma frase");
+            string s = Console.ReadLine();
+            //Pede um char
+            Console.WriteLine("Insere um char");
+            string cSTR = Console.ReadLine();
+            c = cSTR[0];
+
+            //Imprime string
+            //Precorre string
+            foreach (char cInString in s)
+            {
+                if (cInString != c)
+                {
+                    Console.Write(cInString);
+                }
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
