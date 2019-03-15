338ed600c157e1f56392bd2c08075b8d962be3fd
AndreRosa22 <andrefrancorosa@gmail.com>
2019-03-15 09:20:49 +0000

    Adicionei solução para exercicio 1

--- IncDec/Program.cs
+++ IncDec/Program.cs
@@ -6,7 +6,20 @@ namespace IncDec
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 3;
+            int b = 6;
+            int c = a++ + b++;
+            int d = 65;
+            int e = 32;
+            int f = ++d + ++e;
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("c = (a+1) + (b+1) = " + c);
+            Console.WriteLine("d = " + d);
+            Console.WriteLine("e = " + e);
+            Console.WriteLine("f = ++d + ++e = " + f);
+            Console.WriteLine();
+            Console.ReadKey();
         }
     }
 }
